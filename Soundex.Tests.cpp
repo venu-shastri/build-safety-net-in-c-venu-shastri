df36a2d487b319a95b45319d0e53ffcd1f942796
Venugopal K <venu.shastri@gmail.com>
2024-09-19 10:41:27 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,1 @@
-#include <gtest/gtest.h>
-#include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
-// ASSERT_EQ(soundex,"A200");
-}
